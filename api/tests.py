99fe472ba4e8b22d9eca96d8425c268240886e45
Heorhi Khadakovsky <heorhi.khadakovsky@gmail.com>
2018-12-18 16:03:12 +0300

    add some tests

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
+import os
 
-# Create your tests here.
+from celery.result import AsyncResult
+from django.test import SimpleTestCase, override_settings
+from django.urls import reverse
+
+from common.utils import get_pdf_file_path
+
+VALID_WEB_URL = 'https://www.google.com/'
+
+
+class PdfGenerationTestCase(SimpleTestCase):
+    url = reverse("api:pdf_generation")
+
+    def test_invalid_url(self):
+        response = self.client.post(self.url, {"url": "web.telegram213.ci"})
+        self.assertEqual(400, response.status_code)
+
+    @override_settings(CELERY_TASK_ALWAYS_EAGER=True)
+    def test_valid_url(self):
+        response = self.client.post(self.url, {"url": VALID_WEB_URL})
+        task_id = response.json().get('id')
+        self.assertEqual(200, response.status_code)
+
+        task_result = AsyncResult(task_id).get()
+        self.assertTrue(os.path.isfile(get_pdf_file_path(task_result)))
+
+
+class TaskCheckerTestCase(SimpleTestCase):
+    generate_pdf_url = reverse("api:pdf_generation")
+    url = reverse("api:pdf_task_check")
+
+    def test_invalid_query_params(self):
+        response = self.client.get(self.url)
+        self.assertEqual(400, response.status_code)
+
+    @override_settings(CELERY_TASK_ALWAYS_EAGER=True)
+    def test_valid_url_param(self):
+        response = self.client.post(self.generate_pdf_url, {"url": VALID_WEB_URL})
+        task_id = response.json().get('id')
+
+        task_result = AsyncResult(task_id).get()
+        response = self.client.get(self.url + '?task_id={}'.format(task_id))
+        self.assertEqual(task_result, response.json().get('filename'))
+
+
--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from api import views
 
+app_name = 'api'
+
 urlpatterns = [
-    path('pdf/', views.PdfGenerator.as_view()),
-    path('pdf/check_task/', views.PdfTaskChecker.as_view()),
+    path('pdf/', views.PdfGenerator.as_view(), name='pdf_generation'),
+    path('pdf/check_task/', views.PdfTaskChecker.as_view(), name='pdf_task_check'),
 ]
